9e6e493bd0bd37fa213ce6c170204f19a369faf3
Fernando Gilli <fernando@wekers.org>
2023-09-23 10:27:53 -0300

    remove moon ico from moongiant.com, get only at moon.nasa

--- GetMoon.sh
+++ GetMoon.sh
@@ -5,7 +5,7 @@
 # File: GetMoon.sh                                       /\
 # Type: Bash Shell Script                               /_.\
 # By Fernando Gilli fernando<at>wekers(dot)org    _,.-'/ `",\'-.,_
-# Last modified:2023-09-19                     -~^    /______\`~~-^~:
+# Last modified:2023-09-23                     -~^    /______\`~~-^~:
 # ------------------------
 # Get Moon data from moongiant.com
 # / OS : $Linux, $FreeBSD (X Window)
@@ -28,11 +28,8 @@ hemisphere="s"
 
 wget -q -O ${DirShell}/raw "http://www.moongiant.com/phase/today" > /dev/null 2>&1
 
-cp ${DirShell}/raw ${DirShell}/ico
-
 [ -f ${DirShell}/moon_tmp.jpg ] && rm ${DirShell}/moon_tmp.jpg
 [ -f ${DirShell}/moon.jpg ] && rm ${DirShell}/moon.jpg
-
 
 sed -i -e '/^ *$/d' -e 's/^ *//g' ${DirShell}/raw
 sed -i '/Illumination/!d' ${DirShell}/raw
@@ -40,43 +37,23 @@ sed -i 's/<br>/\n/g' ${DirShell}/raw
 sed -i 's|<[^>]*>||g' ${DirShell}/raw
 sed -i -e '4d' ${DirShell}/raw
 
-#ico name
-sed -i '/var jArray=\|"todayMoonContainer"/!d' ${DirShell}/ico
-sed -i -e 's/"\]};//g' -e 's/^.*today_phase\///g' -e 's/\.jpg.*$//g' ${DirShell}/ico
-cat  ${DirShell}/ico >> ${DirShell}/raw
-img_in=$(sed -n 4p ${DirShell}/raw)
-[ -f ${DirShell}/ico ] && rm  ${DirShell}/ico
 now=$(date --date="now" +%H)
 
-
 # Moon image
-if [[ $now >=18 || $now < 06 ]]; then
+wget -q -O ${DirShell}/get_moon_icon_tmp "https://moon.nasa.gov/moon-observation/daily-moon-guide/" > /dev/null 2>&1
+#sed -i -e 's/\(.*\)/\L\1/' ${DirShell}/get_moon_icon_tmp # transform all lowercase
+now_ico="$(LANG=en_us_88591 date +'%d %b %Y')"
   
-  # night moon -> more light
-  wget -q --output-document=${DirShell}/moon_tmp.jpg https://www.moongiant.com/images/today_phase/$img_in.jpg > /dev/null 2>&1
-
-
-else
-
-  # day moon -> more dark
-  wget -q -O ${DirShell}/get_moon_icon_tmp "https://moon.nasa.gov/moon-observation/daily-moon-guide/" > /dev/null 2>&1
-  #sed -i -e 's/\(.*\)/\L\1/' ${DirShell}/get_moon_icon_tmp # transform all lowercase
-
-  now_ico="$(LANG=en_us_88591 date +'%d %b %Y')"
+grep -E -o "${now_ico}.{0,428}" ${DirShell}/get_moon_icon_tmp | \
+sed 's/&quot;/ /g'| sed 's/, /\n/g' | sed -e '2,13d' | sed 's/image_src ://g' | \
+sed 's/^ *//g' | sed 's/\.jpg.*/.jpg/' > ${DirShell}/get_moon_icon
   
-  grep -E -o "${now_ico}.{0,428}" ${DirShell}/get_moon_icon_tmp | \
-  sed 's/&quot;/ /g'| sed 's/, /\n/g' | sed -e '2,13d' | sed 's/image_src ://g' | \
-  sed 's/^ *//g' | sed 's/\.jpg.*/.jpg/' > ${DirShell}/get_moon_icon
+img_ico=$(sed -n 2p ${DirShell}/get_moon_icon)
+
+wget -q --output-document=${DirShell}/moon_tmp.jpg https://moon.nasa.gov/$img_ico > /dev/null 2>&1
+
+[ -f ${DirShell}/get_moon_icon_tmp ] && rm  ${DirShell}/get_moon_icon_tmp
   
-  img_ico=$(sed -n 2p ${DirShell}/get_moon_icon)
-
-  wget -q --output-document=${DirShell}/moon_tmp.jpg https://moon.nasa.gov/$img_ico > /dev/null 2>&1
-
-  [ -f ${DirShell}/get_moon_icon_tmp ] && rm  ${DirShell}/get_moon_icon_tmp
-  
-fi
-
-
 
 # Translate pt-br (moon phase name)
 if [[ $lang == "pt-br" ]]; then
