c2443183340ad9a8bd58bc6ce58d25ab7b23d287
Fernando Gilli <fernando@wekers.org>
2023-09-30 18:59:06 -0300

    Update code to Get next New and Full Moon correctly

--- moon.pl
+++ moon.pl
@@ -7,7 +7,7 @@
 # File: moon.pl                                          /\
 # Type: Perl Script                                     /_.\
 # By Fernando Gilli fernando<at>wekers(dot)org    _,.-'/ `",\'-.,_
-# Last modified:2023-09-21                     -~^    /______\`~~-^~:
+# Last modified:2023-09-30                     -~^    /______\`~~-^~:
 # ------------------------
 # Get Moon data from perl module Astro::MoonPhase
 # / OS : $Linux, $FreeBSD (X Window)
@@ -20,19 +20,85 @@ use POSIX qw(strftime);
 
 use Astro::MoonPhase;
 
+open(my $fh, '<', 'moon_phase_die'); # open file
+  my @list = <$fh>;
+close $fh; # Close file
 
-my @phases = phasehunt();
-#print "New Moon      = ", scalar(localtime($phases[0])), "\n";
-#print "First quarter = ", scalar(localtime($phases[1])), "\n";
-#print "Full moon     = ", scalar(localtime($phases[2])), "\n";
-#print "Last quarter  = ", scalar(localtime($phases[3])), "\n";
-#print "New Moon      = ", scalar(localtime($phases[4])), "\n";
 
-open(my $fh, '>', 'moon_phase_die');
+my $start = time();
+my $stop = $start+30*24*60*60; # Now +30 days
+
+my @name = ("New Moon", "First Quarter", "Full Moon", "Last Quarter");
+(my $phase, my @times) = phaselist($start, $stop);
+ 
+my $countFullMoon = 0;
+my $countNewMoon = 0;
+
+while (@times){
+
+  my $timeNextPhase = scalar shift @times;
+
+  if ($phase eq 2 and $countFullMoon eq 0) { # 2 = Full Moon
+      
+      $countFullMoon = 1; # to execute only one time
+      
+      my $days_left_full = int((($timeNextPhase - time()) ) / 24 / 60 / 60);
+      my $hours_left_full = int((($timeNextPhase - time()) % 86400) / 3600);
+      my $min_left_full = int((($timeNextPhase - time()) % 3600) / 60);
+     
+      # list to write on file
+      $list[0] = "$name[$phase]\n";
+            
+      if($days_left_full eq 0 and $hours_left_full > 0){
+	  $list[1] = "in $hours_left_full hr   \n";
+      }
+      elsif ($days_left_full eq 0 and $hours_left_full eq 0) {
+	  $list[1] = "in $min_left_full m    \n";
+      }
+      elsif ($days_left_full > 0) {
+	  $list[1] = ucfirst strftime("%b %d\n", localtime($timeNextPhase));
+	  #print ucfirst strftime("%b %d %R", gmtime($timeNextPhase)), "\n"; #In GMT Time
+      }
+      
+      #print "days left for full is: $days_left_full", "\n";
+      #print "hour left for full is: $hours_left_full hrs", "\n";
+      #print "min left for full is: $min_left_full m", "\n";
+   
+  }
     
-  print $fh "Full Moon\n";
-  print $fh ucfirst strftime("%b %d", localtime($phases[2])), "\n";
-  print $fh "New Moon\n";
-  print $fh ucfirst strftime("%b %d", localtime($phases[4])), "\n";
+  if ($phase eq 0 and $countNewMoon eq 0) { # 2 = Full Moon
+      
+      $countNewMoon = 1; # to execute only one time
+      
+      my $days_left_new = int((($timeNextPhase - time()) ) / 24 / 60 / 60);
+      my $hours_left_new = int((($timeNextPhase - time()) % 86400) / 3600);
+      my $min_left_new = int((($timeNextPhase - time()) % 3600) / 60);
+     
+      # list to write on file
+      $list[2] = "$name[$phase]\n";
+            
+      if($days_left_new eq 0 and $hours_left_new > 0){
+	  $list[3] = "in $hours_left_new hr   \n";
+      }
+      elsif ($days_left_new eq 0 and $hours_left_new eq 0) {
+	  $list[3] = "in $min_left_new m    \n";
+      }
+      elsif ($days_left_new > 0) {
+	  $list[3] = ucfirst strftime("%b %d\n", localtime($timeNextPhase));
+	  #print ucfirst strftime("%b %d %R", gmtime($timeNextPhase)), "\n"; #In GMT Time
+      }
+      
+      #print "days left for new is: $days_left_new", "\n";
+      #print "hour left for new is: $hours_left_new hrs", "\n";
+      #print "min left for new is: $min_left_new m", "\n";
+ 
+  }
 
-close $fh;
+$phase = ($phase + 1) % 4;
+
+open(my $fh, '>', 'moon_phase_die'); # open file
+  print $fh @list; # print list on file
+close $fh; # Close file
+
+} # Close while
+
