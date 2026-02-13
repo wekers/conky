af80cf478bdc574e174ec77ef3304825488b109e
Fernando Gilli <fernando@wekers.org>
2026-02-13 17:41:52 -0300

    feat: update conky script to launch 2K configuration and comment out 1080p option

--- conky.sh
+++ conky.sh
@@ -11,7 +11,8 @@ if [ "$?" -eq 0 ]; then #igual
     echo "conky is already running. PID $Pid"
     exit 0
 else
-   sleep 5 && conky -c /home/fernando/.conky/wekers/.conkyrc &
+   #sleep 5 && conky -c /home/fernando/.conky/wekers/.conkyrc_1080p &
+   sleep 5 && conky -c /home/fernando/.conky/wekers/conkyrc/.conkyrc_2k &
    #sleep 5 && nice conky -c /home/fernando/.conky/wekers/.conkyrc &
    exit 0
 fi
