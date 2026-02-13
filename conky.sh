74a37e72b308c8230f1812cfc1e211a3a3d12e8f
Fernando Gilli <fernando@wekers.org>
2026-02-12 22:03:11 -0300

    feat: add new font files and conky script for enhanced display options

---
+++ conky.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#sleep 2
+#killall -9 conky  
+
+sleep 2
+Pid=$(pidof conky  )
+ps --pid "$Pid"  > /dev/null 2>&1
+# shellcheck disable=SC2181
+if [ "$?" -eq 0 ]; then #igual
+    echo "conky is already running. PID $Pid"
+    exit 0
+else
+   sleep 5 && conky -c /home/fernando/.conky/wekers/.conkyrc &
+   #sleep 5 && nice conky -c /home/fernando/.conky/wekers/.conkyrc &
+   exit 0
+fi
